8a6e4f3f5bff9760ec815c3cbbe89dd6f61003f5
Alexandre Dagnelies <alexandre.dagnelies@koben.fr>
2016-05-03 16:15:37 +0200

    add var to script

--- src/application.sh
+++ src/application.sh
@@ -1,6 +1,6 @@
 ### application.sh - 2016 - Alexandre Dagnelies ###
-echo "Copyright company - 2016"
-echo "Build : 10:00"
-echo "version 1.1"
+version = "2"
+build_hour = "11H00"
 
-
+echo "Build ${build_hour}"
+echo "version ${version}"
